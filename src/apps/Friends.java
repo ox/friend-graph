b74be95ea51470d81e419018dfa0cab6950f871b
Julian Modesto <julianvmodesto@gmail.com>
2012-04-26 18:17:24 -0400

    menu

--- src/apps/Friends.java
+++ src/apps/Friends.java
@@ -2,13 +2,19 @@ package apps;
 
 public class Friends {
 	public static void main(String[] args) {
-		
+		//read graph file
+		//build a graph
 		System.out.print("\tChoose action: ");
 		System.out.print("students at school");
 		System.out.print("shortest intro chain");
 		System.out.print("cliques at school");
 		System.out.print("connectors");
 		System.out.print("(q)uit=>");
-
+		char response = stdin.next().toLowerCase().charAt(0);
+		while (response=='s') {
+			
+		}
 	}
+	
+	
 }
