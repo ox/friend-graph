5e6a7e3c40269906eff7aebf86cc47d14a74a819
Artem Titoulenko <artem.titoulenko@gmail.com>
2012-04-26 18:17:11 -0400

    read file

--- src/apps/Friends.java
+++ src/apps/Friends.java
@@ -1,9 +1,16 @@
 package apps;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Friends {
-	public static void main(String[] args) {
-		System.out.println("goodbye world");
-		System.out.println("derp");
-		System.out.println("herp");
+	public static void main(String[] args) throws FileNotFoundException {
+		Scanner scstdin = new Scanner(System.in);
+		
+		System.out.println("graph file: ");
+		String line = scstdin.next();
+
+		Scanner scfile = new Scanner(new File(line));
 	}
 }
